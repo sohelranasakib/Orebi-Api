c657c53b7ea8bf4ed3bae177c1d3fdbb8b9fff45
sohelranasakib <sohelrany802@gmail.com>
2024-09-13 14:30:02 +0600

    demo

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Container from './Container'
+import Flex from './Flex'
+
+const Header = () => {
+  return (
+    <section>
+        <Container>
+            <Flex>
+            <h2>h</h2>
+            <h2>j</h2>
+            <h2>d</h2>
+            </Flex>
+        </Container>
+    </section>
+   
+  )
+}
+
+export default Header
